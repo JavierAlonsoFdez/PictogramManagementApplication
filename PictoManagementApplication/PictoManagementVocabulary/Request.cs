fdc4018cd6e7c3c933eedeed47806a0e14471c78
Javier Alonso <46961426+uo243620@users.noreply.github.com>
2020-04-09 12:53:30 +0200

    Request message included

---
+++ PictoManagementApplication/PictoManagementVocabulary/Request.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PictoManagementVocabulary
+{
+    /// <summary>
+    /// Petición que realizará el usuario contra el servidor
+    /// </summary>
+    [Serializable()]
+    public class Request
+    {
+        private string _type;
+        private string _requestBody;
+
+        /// <summary>
+        /// Método público para acceder a la propiedad privada _type
+        /// </summary>
+        public string Type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+
+        /// <summary>
+        /// Método para acceder a la propiedad privada _requestBody
+        /// </summary>
+        public string RequestBody
+        {
+            get { return _requestBody; }
+            set { _requestBody = value; }
+        }
+
+        /// <summary>
+        /// Constructor de la clase
+        /// </summary>
+        /// <param name="NewType">Tipo del objeto solicitado</param>
+        /// <param name="RequestBody">Cuerpo de la petición</param>
+        public Request(string NewType, string RequestBody)
+        {
+            _type = NewType;
+            _requestBody = RequestBody;
+        }
+    }
+}
---
+++ PictoManagementApplication/PictoManagementVocabularyUnitTest/RequestUnitTest.cs
@@ -0,0 +1,218 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PictoManagementVocabulary;
+using System;
+
+namespace PictoManagementVocabularyUnitTest
+{
+    [TestClass]
+    public class RequestUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Request request = new Request("Image", "Payload for testing");
+            BinaryCodec<Request> binCod = new BinaryCodec<Request>();
+
+            byte[] reqCod = binCod.Encode(request);
+
+            Request reqDecod = binCod.Decode(reqCod);
+
+            Assert.AreEqual(request.Type, reqDecod.Type);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Request request = new Request("Image", "Payload for testing");
+            BinaryCodec<Request> binCod = new BinaryCodec<Request>();
+
+            byte[] reqCod = binCod.Encode(request);
+
+            Request reqDecod = binCod.Decode(reqCod);
+
+            Assert.AreEqual(request.RequestBody, reqDecod.RequestBody);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Request request = new Request("", "");
+            BinaryCodec<Request> binCod = new BinaryCodec<Request>();
+
+            byte[] reqCod = binCod.Encode(request);
+
+            Request reqDecod = binCod.Decode(reqCod);
+
+            Assert.AreEqual(request.Type, reqDecod.Type);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Request request = new Request("", "");
+            BinaryCodec<Request> binCod = new BinaryCodec<Request>();
+
+            byte[] reqCod = binCod.Encode(request);
+
+            Request reqDecod = binCod.Decode(reqCod);
+
+            Assert.AreEqual(request.RequestBody, reqDecod.RequestBody);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Request request = new Request("Image", "");
+            BinaryCodec<Request> binCod = new BinaryCodec<Request>();
+
+            byte[] reqCod = binCod.Encode(request);
+
+            Request reqDecod = binCod.Decode(reqCod);
+
+            Assert.AreEqual(request.Type, reqDecod.Type);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Request request = new Request("Image", "");
+            BinaryCodec<Request> binCod = new BinaryCodec<Request>();
+
+            byte[] reqCod = binCod.Encode(request);
+
+            Request reqDecod = binCod.Decode(reqCod);
+
+            Assert.AreEqual(request.RequestBody, reqDecod.RequestBody);
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Request request = new Request("", "Payload for testing");
+            BinaryCodec<Request> binCod = new BinaryCodec<Request>();
+
+            byte[] reqCod = binCod.Encode(request);
+
+            Request reqDecod = binCod.Decode(reqCod);
+
+            Assert.AreEqual(request.Type, reqDecod.Type);
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            Request request = new Request("", "Payload for testing");
+            BinaryCodec<Request> binCod = new BinaryCodec<Request>();
+
+            byte[] reqCod = binCod.Encode(request);
+
+            Request reqDecod = binCod.Decode(reqCod);
+
+            Assert.AreEqual(request.RequestBody, reqDecod.RequestBody);
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+            Request request = new Request("Image", "Payload for testing");
+            JsonCodec<Request> jsonCod = new JsonCodec<Request>();
+
+            byte[] reqCod = jsonCod.Encode(request);
+
+            Request reqDecod = jsonCod.Decode(reqCod);
+
+            Assert.AreEqual(request.Type, reqDecod.Type);
+        }
+
+        [TestMethod]
+        public void TestMethod10()
+        {
+            Request request = new Request("Image", "Payload for testing");
+            JsonCodec<Request> jsonCod = new JsonCodec<Request>();
+
+            byte[] reqCod = jsonCod.Encode(request);
+
+            Request reqDecod = jsonCod.Decode(reqCod);
+
+            Assert.AreEqual(request.RequestBody, reqDecod.RequestBody);
+        }
+
+        [TestMethod]
+        public void TestMethod11()
+        {
+            Request request = new Request("", "");
+            JsonCodec<Request> jsonCod = new JsonCodec<Request>();
+
+            byte[] reqCod = jsonCod.Encode(request);
+
+            Request reqDecod = jsonCod.Decode(reqCod);
+
+            Assert.AreEqual(request.Type, reqDecod.Type);
+        }
+
+        [TestMethod]
+        public void TestMethod12()
+        {
+            Request request = new Request("", "");
+            JsonCodec<Request> jsonCod = new JsonCodec<Request>();
+
+            byte[] reqCod = jsonCod.Encode(request);
+
+            Request reqDecod = jsonCod.Decode(reqCod);
+
+            Assert.AreEqual(request.RequestBody, reqDecod.RequestBody);
+        }
+
+        [TestMethod]
+        public void TestMethod13()
+        {
+            Request request = new Request("Image", "");
+            JsonCodec<Request> jsonCod = new JsonCodec<Request>();
+
+            byte[] reqCod = jsonCod.Encode(request);
+
+            Request reqDecod = jsonCod.Decode(reqCod);
+
+            Assert.AreEqual(request.Type, reqDecod.Type);
+        }
+
+        [TestMethod]
+        public void TestMethod14()
+        {
+            Request request = new Request("Image", "");
+            JsonCodec<Request> jsonCod = new JsonCodec<Request>();
+
+            byte[] reqCod = jsonCod.Encode(request);
+
+            Request reqDecod = jsonCod.Decode(reqCod);
+
+            Assert.AreEqual(request.RequestBody, reqDecod.RequestBody);
+        }
+
+        [TestMethod]
+        public void TestMethod15()
+        {
+            Request request = new Request("", "Payload for testing");
+            JsonCodec<Request> jsonCod = new JsonCodec<Request>();
+
+            byte[] reqCod = jsonCod.Encode(request);
+
+            Request reqDecod = jsonCod.Decode(reqCod);
+
+            Assert.AreEqual(request.Type, reqDecod.Type);
+        }
+
+        [TestMethod]
+        public void TestMethod16()
+        {
+            Request request = new Request("", "Payload for testing");
+            JsonCodec<Request> jsonCod = new JsonCodec<Request>();
+
+            byte[] reqCod = jsonCod.Encode(request);
+
+            Request reqDecod = jsonCod.Decode(reqCod);
+
+            Assert.AreEqual(request.RequestBody, reqDecod.RequestBody);
+        }
+    }
+}
