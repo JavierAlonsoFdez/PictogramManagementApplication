2a0a1a12682cbf0760c766a95bfd857a70734e2b
Javier Alonso <46961426+uo243620@users.noreply.github.com>
2020-05-10 11:23:03 +0200

    Deleted project GUI and updated to Client

---
+++ PictoManagementApplication/PictoManagementGUI/Model/Image.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictoManagementGUI.Model
+{
+    public class Image
+    {
+        public int ID { get; set; }
+        [Display(Name = "Título")]
+        public string title { get; set; }
+
+    }
+}
