5413abb2b3a0809a60a2735f408c2774de85a140
Javier Alonso <46961426+uo243620@users.noreply.github.com>
2020-05-25 18:00:29 +0200

    Change framework of client test

---
+++ PictoManagementApplication/PictoManagementClientTest/Receiver.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using PictoManagementVocabulary;
+
+namespace PictoManagementClientTest
+{
+    public class Receiver
+    {
+        TcpClient clientTest;
+        BinaryReader binReader;
+        public byte[] rcvBuffer;
+        public BinaryCodec<Image> binCodImage = new BinaryCodec<Image>();
+        public BinaryCodec<Dashboard> binCodDash = new BinaryCodec<Dashboard>();
+
+        public Receiver(TcpClient client, BinaryReader br)
+        {
+            try
+            {
+                clientTest = client;
+                binReader = br;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + " at " + e.StackTrace);
+                throw e;
+            }
+        }
+
+        public void ReceiveImage()
+        {
+            using (binReader)
+            {
+                int bufferSize = binReader.ReadInt32();
+                rcvBuffer = binReader.ReadBytes(bufferSize);
+            }
+            Image imageReceived = binCodImage.Decode(rcvBuffer);
+        }
+
+        public void ReceiveDashboard()
+        {
+            using (binReader)
+            {
+                int bufferSize = binReader.ReadInt32();
+                rcvBuffer = binReader.ReadBytes(bufferSize);
+            }
+            Dashboard dashboard = binCodDash.Decode(rcvBuffer);
+        }
+    }
+}
